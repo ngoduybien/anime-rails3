80039d6482bc9ce0192ff12f5a3720468317f6d9
ngoduybien <shinichi.bien@gmail.com>
2013-01-21 16:42:24 -0500

    add login

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 AnimeRails3::Application.routes.draw do
+  resources :users
+
+  root :to => 'home#index'
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -55,4 +59,9 @@ AnimeRails3::Application.routes.draw do
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id))(.:format)'
+
+  match '/auth/:provider/callback' => 'sessions#create'
+  match '/auth/failure' => 'sessions#failure'
+  match '/signout' => 'sessions#destroy', :as => :signout
+  match '/signin' => 'sessions#new', :as => :signin
 end
