1f20970a846e2cccd4055f8e550ec3bc608d4f84
ngoduybien <shinichi.bien@gmail.com>
2013-01-21 14:35:43 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AnimeRails3::Application.config.secret_token = 'eb7c0d69b38943ba42898123f317d4a50030e35d383563df9a32f34d4193c23c2f363a6dc94267ffbb0b329a512ce25916f7275ec35df72815d02a2f74bad77c'
